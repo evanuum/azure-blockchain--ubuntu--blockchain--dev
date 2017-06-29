0fa8502907ad3329324ce70e248d9f00869c89a1
Eric van Uum <evanuum@microsoft.com>
2017-06-29 15:43:44 +0200

    Updated sh script

--- scripts/installbcdevvm.sh
+++ scripts/installbcdevvm.sh
@@ -176,7 +176,7 @@ sudo npm install -g npm
 sudo npm install -g truffle@beta
 sudo npm install -g ethereumjs-testrpc@beta
 # install parity
-time sudo -H -u $AZUREUSER bash -c 'bash <(curl https://get.parity.io -Lk)'
+time sudo -H -u $AZUREUSER bash -c 'bash <(curl https://raw.githubusercontent.com/paritytech/scripts/master/get-parity.sh -Lk)'
 date
 
 # end of install
